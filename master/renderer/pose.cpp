d18737d11c915cbf7e29ae4ecfca365071b5fc26
Tron2016“ <3632065@qq.com>
2017-06-09 22:25:16 +0800

    add master

---
+++ master/renderer/pose.cpp
@@ -0,0 +1,16 @@
+#include "pose.h"
+Pose::Pose(double elevation, double azimuth, double yaw, double camDistance) {
+
+}
+Pose::Pose() {
+
+}
+double Pose::getDistance(Pose *other) const {
+
+}
+double Pose::degreesToRadians(double degrees) {
+
+}
+double Pose::radiansToDegrees(double radians) {
+
+}
